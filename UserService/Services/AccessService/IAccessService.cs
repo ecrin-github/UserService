6cacd9d76f95e41d73755167cb8fd210dcb93789
Sergey Goryanin <outside11@ntsmail.ru>
2023-04-26 11:17:45 +0400

    Create IAccessService.cs

---
+++ UserService/Services/AccessService/IAccessService.cs
@@ -0,0 +1,8 @@
+﻿using UserService.Contracts.Access;
+
+namespace UserService.Services.AccessService;
+
+public interface IAccessService
+{
+    Task<AccessResponse> GetUserAccessDataAsync(string id);
+}
